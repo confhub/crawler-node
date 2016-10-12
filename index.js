75a01e94ba762aaca8a87bc2cc00c4d44a6373ac
ericyan <eric7578@gmail.com>
2016-10-12 21:53:16 +0800

    add parsing pagination

--- index.js
+++ index.js
@@ -1,26 +1,69 @@
 import { create } from 'phantom';
+import { parse, format } from 'url';
+
+import { delay } from './src/crawler.js';
 
 const entryUrl = 'http://www.ithome.com.tw/seminar_list';
+const pause = delay(200, 500);
 
 entry(entryUrl);
 
-async function entry(targetUrl) {
+async function entry(entry) {
+  const entryUrlObj = parse(entry);
+
+  let urls = [entry];
+  const pathSet = new Set([entryUrlObj.path]);
+
+  while (urls.length > 0) {
+    const targetUrl = urls.pop();
+    const targetUrlObj = parse(targetUrl);
+
+    const parsedUrls = await crawl(targetUrl);
+    if (!!parsedUrls) {
+      const crawlUrls = (Array.isArray(parsedUrls) ? parsedUrls : [parsedUrls])
+      .map(link => parse(link))
+      .filter(urlObj => {
+        const { host } = entryUrlObj;
+        const isSameHost = !urlObj.host || urlObj.host === host;
+        const isNew = !pathSet.has(urlObj.path);
+        return isSameHost && isNew;
+      })
+      .map(urlObj => {
+        const { protocol, auth, host } = entryUrlObj;
+        urlObj.protocol = protocol;
+        urlObj.auth = auth;
+        urlObj.host = host;
+        pathSet.add(urlObj.path);
+        return format(urlObj);
+      });
+
+      urls = urls.concat(crawlUrls);
+
+      console.log(`Parse ${targetUrl}, add ${crawlUrls.length} url(s).`);
+      console.log(`Remain ${urls.length} url(s).`);
+      console.log();
+    }
+
+    await pause();
+  }
+}
+
+async function crawl(targetUrl) {
   let phantom;
-  const { log } = console;
-  const slient = () => {};
+  let otherPages;
   try {
     phantom = await create();
     const page = await phantom.createPage();
     const status = await page.open(targetUrl);
 
     const channelItems = await page.evaluate(getChannelItems);
-    console.log(channelItems);
-
+    otherPages = await page.evaluate(getPaginations);
   } catch (err) {
     console.error(err);
   } finally {
     phantom.exit();
   }
+  return otherPages;
 }
 
 function getChannelItems() {
@@ -38,4 +81,16 @@ function getChannelItems() {
     });
   }
   return ret;
+}
+
+function getPaginations() {
+  const ret = {};
+  const paginationNodes = document.querySelectorAll('.pagination a');
+  for (let i = 0; i < paginationNodes.length; i++) {
+    const paginationNode = paginationNodes[i];
+    const url = paginationNode.getAttribute('href');
+    ret[url] = 1;
+  }
+
+  return Object.keys(ret);
 }
